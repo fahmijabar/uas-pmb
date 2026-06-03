fb3a32c0cd1c2c8bf531c967b6e5ae2ca286b776
febri <febriiar99@upi.edu>
2026-06-03 22:08:43 +0700

    febri

--- lib/screens/tambah_bahan_screen.dart
+++ lib/screens/tambah_bahan_screen.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import '../services/api_services.dart';
-=======
-import 'package:http/http.dart' as http;
->>>>>>> 85ff7d9dcfd194849ca9378ca9bab21f76904b05
 
 class TambahBahanScreen extends StatefulWidget {
   const TambahBahanScreen({super.key});
@@ -44,44 +40,30 @@ class _TambahBahanScreenState extends State<TambahBahanScreen> {
       context: context,
       initialDate: DateTime.now(),
       firstDate: DateTime.now(),
-<<<<<<< HEAD
       lastDate: DateTime(2035),
-=======
-      lastDate: DateTime(2030),
->>>>>>> 85ff7d9dcfd194849ca9378ca9bab21f76904b05
     );
 
     if (picked != null) {
       setState(() {
         _finalKadaluarsa = picked;
-<<<<<<< HEAD
 
-=======
->>>>>>> 85ff7d9dcfd194849ca9378ca9bab21f76904b05
         _tanggalController.text =
             "${picked.year}-${picked.month.toString().padLeft(2, '0')}-${picked.day.toString().padLeft(2, '0')}";
       });
     }
   }
 
-<<<<<<< HEAD
   Future<void> simpanData() async {
     if (!_formKey.currentState!.validate()) return;
 
     setState(() {
       isLoading = true;
     });
-=======
-  Future<void> _simpanData() async {
-    if (!_formKey.currentState!.validate()) return;
-
-    DateTime waktuHitungSistem = DateTime.now();
->>>>>>> 85ff7d9dcfd194849ca9378ca9bab21f76904b05
 
     int masaSimpan = 0;
 
-    if (_selectedJenis == 'Makanan Kemasan (Input Manual)') {
-<<<<<<< HEAD
+    if (_selectedJenis == 'Makanan Kemasan (Input Manual)' ||
+        _selectedJenis == 'Minuman Kemasan (Input Manual)') {
       masaSimpan = _finalKadaluarsa!.difference(DateTime.now()).inDays + 1;
 
       if (masaSimpan <= 0) {
@@ -124,51 +106,6 @@ class _TambahBahanScreenState extends State<TambahBahanScreen> {
           content: Text("Gagal menyimpan data"),
           backgroundColor: Colors.red,
         ),
-=======
-      waktuHitungSistem = _finalKadaluarsa!;
-      masaSimpan = _finalKadaluarsa!.difference(DateTime.now()).inDays;
-    } else if (_selectedJenis == 'Minuman Kemasan (Input Manual)') {
-      waktuHitungSistem = _finalKadaluarsa!;
-      masaSimpan = _finalKadaluarsa!.difference(DateTime.now()).inDays;
-    } else if (_selectedJenis == 'Sayuran (Otomatis 5 Hari)') {
-      masaSimpan = 5;
-      waktuHitungSistem = waktuHitungSistem.add(const Duration(days: 5));
-    } else if (_selectedJenis == 'Buah (Otomatis 7 Hari)') {
-      masaSimpan = 7;
-      waktuHitungSistem = waktuHitungSistem.add(const Duration(days: 7));
-    } else if (_selectedJenis == 'Daging / Ikan (Otomatis 3 Hari)') {
-      masaSimpan = 3;
-      waktuHitungSistem = waktuHitungSistem.add(const Duration(days: 3));
-    }
-    String tanggalMasuk =
-        "${DateTime.now().year}-${DateTime.now().month.toString().padLeft(2, '0')}-${DateTime.now().day.toString().padLeft(2, '0')}";
-
-    try {
-      var response = await http.post(
-        Uri.parse("http://localhost/api_kulkas/create.php"),
-        body: {
-          "nama": _namaController.text,
-          "tanggal_masuk": tanggalMasuk,
-          "masa_simpan": masaSimpan.toString(),
-        },
-      );
-
-      if (response.statusCode == 200) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text('Berhasil! ${_namaController.text} disimpan'),
-            backgroundColor: Colors.green,
-          ),
-        );
-
-        Navigator.pop(context, true); // kembali ke dashboard
-      } else {
-        throw Exception("Gagal simpan");
-      }
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Error: $e"), backgroundColor: Colors.red),
->>>>>>> 85ff7d9dcfd194849ca9378ca9bab21f76904b05
       );
     }
   }
@@ -189,7 +126,6 @@ class _TambahBahanScreenState extends State<TambahBahanScreen> {
               children: [
                 TextFormField(
                   controller: _namaController,
-<<<<<<< HEAD
                   decoration: const InputDecoration(
                     labelText: "Nama Bahan",
                     border: OutlineInputBorder(),
@@ -228,7 +164,8 @@ class _TambahBahanScreenState extends State<TambahBahanScreen> {
                     setState(() {
                       _selectedJenis = value;
 
-                      if (value != 'Makanan Kemasan (Input Manual)') {
+                      if (value != 'Makanan Kemasan (Input Manual)' &&
+                          value != 'Minuman Kemasan (Input Manual)') {
                         _tanggalController.clear();
                         _finalKadaluarsa = null;
                       }
@@ -244,7 +181,8 @@ class _TambahBahanScreenState extends State<TambahBahanScreen> {
 
                 const SizedBox(height: 15),
 
-                if (_selectedJenis == 'Makanan Kemasan (Input Manual)')
+                if (_selectedJenis == 'Makanan Kemasan (Input Manual)' ||
+                    _selectedJenis == 'Minuman Kemasan (Input Manual)')
                   TextFormField(
                     controller: _tanggalController,
                     readOnly: true,
@@ -260,59 +198,10 @@ class _TambahBahanScreenState extends State<TambahBahanScreen> {
                       }
                       return null;
                     },
-=======
-                  decoration: const InputDecoration(labelText: 'Nama Bahan'),
-                  validator: (value) =>
-                      value!.isEmpty ? 'Tidak boleh kosong' : null,
-                ),
-
-                const SizedBox(height: 16),
-
-                TextFormField(
-                  controller: _jumlahController,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(labelText: 'Jumlah'),
-                  validator: (value) =>
-                      value!.isEmpty ? 'Tidak boleh kosong' : null,
-                ),
-
-                const SizedBox(height: 16),
-
-                DropdownButtonFormField<String>(
-                  initialValue: _selectedJenis,
-                  hint: const Text("Pilih jenis"),
-                  items: _listJenis.map((e) {
-                    return DropdownMenuItem(value: e, child: Text(e));
-                  }).toList(),
-                  onChanged: (val) {
-                    setState(() {
-                      _selectedJenis = val;
-                      _tanggalController.clear();
-                    });
-                  },
-                  validator: (value) => value == null ? 'Pilih jenis' : null,
-                ),
-
-                const SizedBox(height: 16),
-
-                if (_selectedJenis == 'Makanan Kemasan (Input Manual)' ||
-                    _selectedJenis == 'Minuman Kemasan (Input Manual)')
-                  TextFormField(
-                    controller: _tanggalController,
-                    readOnly: true,
-                    decoration: const InputDecoration(
-                      labelText: 'Tanggal Kadaluarsa',
-                      suffixIcon: Icon(Icons.calendar_today),
-                    ),
-                    onTap: () => _pilihTanggal(context),
-                    validator: (value) =>
-                        value!.isEmpty ? 'Wajib isi tanggal' : null,
->>>>>>> 85ff7d9dcfd194849ca9378ca9bab21f76904b05
                   ),
 
                 const SizedBox(height: 30),
 
-<<<<<<< HEAD
                 SizedBox(
                   width: double.infinity,
                   height: 50,
@@ -328,11 +217,6 @@ class _TambahBahanScreenState extends State<TambahBahanScreen> {
                             style: TextStyle(color: Colors.white, fontSize: 16),
                           ),
                   ),
-=======
-                ElevatedButton(
-                  onPressed: _simpanData,
-                  child: const Text("Simpan"),
->>>>>>> 85ff7d9dcfd194849ca9378ca9bab21f76904b05
                 ),
               ],
             ),
